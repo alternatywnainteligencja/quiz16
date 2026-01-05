663384883c4a85e658a9b6f36d0ecd74faa635f3
alternatywnainteligencja <alternatywnainteligencja@gmail.com>
2026-01-05 19:10:14 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/quiz16/', // 👈 bardzo ważne dla GitHub Pages
-});
