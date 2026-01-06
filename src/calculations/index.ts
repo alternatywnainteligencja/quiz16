3ac304fae5eefd7354e393989ae3197ea67a273c
alternatywnainteligencja <alternatywnainteligencja@gmail.com>
2026-01-06 10:03:17 +0100

    Create index.ts

---
+++ src/calculations/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Index file - re-exportuje główne funkcje
+ * Dzięki temu importy w całej aplikacji pozostają bez zmian
+ */
+
+export * from './calculations';
+export type { CalculationResult } from './calculations';
+export type { AnalysisResult } from './analysisEngine';
