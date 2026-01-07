d81ef132c3382344c47fc08b089c616d8eaf9d27
alternatywnainteligencja <alternatywnainteligencja@gmail.com>
2026-01-07 20:43:31 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import MarriageQuiz from './components/MarriageQuiz';
-
 function App() {
-  return <MarriageQuiz />;
+  return <div style={{ padding: '2rem', fontSize: '2rem' }}>
+    🔥 TEST - App.tsx działa!
+  </div>;
 }
-
 export default App;
+
+#import MarriageQuiz from './components/MarriageQuiz';
+#
+#function App() {
+#  return <MarriageQuiz />;
+#}
+#
+#export default App;
